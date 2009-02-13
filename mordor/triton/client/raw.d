8f8a33352dc83ba7dddc9cde8cbd314968a7ec21
Cody Cutrer <cody@mozy.com>
2009-02-13 16:54:14 -0700

    Refactor raw to be a separate function from main, using LimitedStream to return the stream.

--- mordor/triton/client/raw.d
+++ mordor/triton/client/raw.d
@@ -7,10 +7,36 @@ import tango.util.Convert;
 import mordor.common.asyncsocket;
 import mordor.common.iomanager;
 import mordor.common.streams.buffered;
+import mordor.common.streams.limited;
 import mordor.common.streams.socket;
 import mordor.common.streams.fd;
 import mordor.common.streams.streamtostream;
 import mordor.common.stringutils;
+
+result_t raw(BufferedStream tds, string objectName, out Stream object)
+{
+    char[] command = "raw\nb3b83038ce5abfc071828af9e24d944f\n"
+        ~ objectName ~ "\n0\n";
+    
+    result_t result = tds.write(command);
+    if (result < 0) {
+        return result;        
+    }
+    result = tds.flush();
+    if (result < 0) {
+        return result;        
+    }
+    
+    char[] line;
+    result = tds.getDelimited(line);
+    
+    if (line.length >= 5 && line[0..5] == "ERROR") {
+        throw new Exception(line);        
+    }
+    long length = to!(long)(line);
+    object = new LimitedStream(tds, length, false);
+    return 0;
+}
 
 int main(string[] args)
 {
@@ -22,37 +48,16 @@ int main(string[] args)
         
         AsyncSocket socket = new AsyncSocket(ioManager, AddressFamily.INET, SocketType.STREAM, ProtocolType.TCP);
         socket.connect(new InternetAddress(args[1], to!(int)(args[2])));
-        BufferedStream triton = new BufferedStream(new SocketStream(socket));
+        BufferedStream tds = new BufferedStream(new SocketStream(socket));
         Stream stdout = new FDStream(1);
+        Stream object;
         
-        char[] command = "raw\nb3b83038ce5abfc071828af9e24d944f\n".dup;
-        command ~= args[3];
-        command ~= "\n0\n";
-        
-        result_t result = triton.write(command);
-        if (result < 0) {
-            Stderr.formatln("Failed to send command: {}", result);
+        result_t result = raw(tds, args[3], object);
+        if (result != 0) {
+            Stderr.formatln("Failed to communicate with triton: {}", result);
             return;
         }
-        result = triton.flush();
-        if (result < 0) {
-            Stderr.formatln("Failed to send command: {}", result);
-            return;
-        }
-        
-        char[] line;
-        result = triton.getDelimited(line);
-        if (result < 0) {
-            Stderr.formatln("Failed to read triton's response: {}", result);
-            return;
-        }
-        if (line.length >= 5 && line[0..5] == "ERROR") {
-            Stderr.formatln("{}", line);
-            return;
-        }
-        size_t length = to!(size_t)(line);
-        long transferred;
-        result = streamToStream(triton, stdout, transferred, length);
+        result = streamToStream(object, stdout);
         if (result < 0) {
             Stderr.formatln("Failed to read object from triton: {}", result);
             return;
