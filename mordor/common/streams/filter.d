9e83c0bfcdbccaf0bbe001369c86e235edd3df7f
Cody Cutrer <cody@mozy.com>
2009-02-12 23:19:54 -0700

    FilterStream

---
+++ mordor/common/streams/filter.d
@@ -0,0 +1,47 @@
+module mordor.common.streams.filter;
+
+public import mordor.common.streams.stream;
+
+class FilterStream : Stream
+{
+protected:
+    this(Stream parent, bool ownsParent = true)
+    {
+        _parent = parent;
+        _ownsParent = ownsParent;
+    }
+    
+public:
+    Stream parent() { return _parent; }
+    bool ownsParent() { return _ownsParent; }
+    
+    bool supportsRead() { return _parent.supportsRead; }
+    bool supportsWrite() { return _parent.supportsWrite; }
+    bool supportsSeek() { return _parent.supportsSeek; }
+    bool supportsSize() { return _parent.supportsSize; }
+    bool supportsTruncate() { return _parent.supportsTruncate; }
+    bool supportsEof() { return _parent.supportsEof; }
+    
+    result_t close(CloseType type = CloseType.BOTH)
+    {
+        if (ownsParent)
+            return _parent.close(type);
+        else
+            return 0;
+    }
+    result_t read(Buffer b, size_t len) { return _parent.read(b, len); }
+    result_t write(Buffer b, size_t len) { return _parent.write(b, len); }
+    result_t seek(long offset, Anchor anchor, ref long size) { return _parent.seek(offset, anchor, size); }
+    result_t size(ref long size) { return _parent.size(size); }
+    result_t truncate(long size) { return _parent.truncate(size); }
+    result_t flush() { return _parent.flush(); }
+    result_t eof() { return _parent.eof(); }
+    
+protected:
+    Stream parent(Stream parent) { return _parent = parent; }
+    bool ownsParent(bool ownsParent) { _ownsParent = ownsParent; }
+    
+private:
+    Stream _parent;
+    bool _ownsParent;
+}
