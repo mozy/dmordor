deb02a9c2183ac2ee022e2a5b42137b5ed80fd69
Cody Cutrer <cody@mozy.com>
2009-02-20 16:17:24 -0700

    Add notifications, to/from string (needed for loading without knowing the type), and loadFromEnvironment for config.

--- mordor/common/config.d
+++ mordor/common/config.d
@@ -1,6 +1,9 @@
 module mordor.common.config;
 
+import tango.text.Ascii;
+import tango.text.Util;
 import tango.util.Convert;
+import tango.sys.Environment;
 
 import mordor.common.stringutils;
 
@@ -11,6 +14,12 @@ public:
     string description() { return _description; }
     bool dynamic() { return _dynamic; }
     bool automatic() { return _automatic; }
+    
+    void monitor(void delegate() dg) { _dgs ~= dg; }
+    void monitor(void function() fn) { _fns ~= fn; }
+    
+    abstract string toString();
+    abstract void fromString(string v);
 
 protected:
     this(string name, string description, bool dynamic,
@@ -27,6 +36,10 @@ private:
     string _description;
     bool _dynamic;
     bool _automatic;
+    
+protected:
+    void delegate()[] _dgs;
+    void function()[] _fns;
 }
 
 class ConfigVar(T) : public ConfigVarBase
@@ -37,15 +50,38 @@ public:
         bool dynamic = true, bool automatic = false)
     {
         super(name, description, dynamic, automatic);
-        val = defaultValue;
+        static if (T.sizeof > size_t.sizeof) {
+            _val = new Box(defaultValue);
+        } else {
+            _val = defaultValue;
+        }
     }
+    
+    string toString() { return to!(string)(val); }
+    void fromString(string v) { val = to!(T)(v); }
 
     static if (T.sizeof > size_t.sizeof) {
         /* invariant */ T val() { volatile return _val.val; }
-        void val(/* invariant */ T v) { volatile _val = new Box(v); }
+        void val(/* invariant */ T v)
+        {
+            // TODO: atomicCompareExchange or something
+            T oldVal = val;
+            volatile _val = new Box(v);
+            if (oldVal != val) {
+                notify(oldVal);
+            }
+        }
     } else {
         /* invariant */ T val() { volatile return _val; }
-        void val(/* invariant */ T v) { volatile _val = v; }
+        void val(/* invariant */ T v)
+        {
+            // TODO: atomicCompareExchange or something
+            T oldVal = val;
+            volatile _val = v;
+            if (oldVal != val) {
+                notify(oldVal);
+            }
+        }
     }
 
 private:
@@ -59,6 +95,20 @@ private:
     } else {
         T _val;
     }
+    
+    void notify(T oldVal)
+    {
+        foreach(dg; _dgs) {
+            dg();
+            if (oldVal != val) 
+                return;
+        }
+        foreach(fn; _fns) {
+            fn();
+            if (oldVal != val)
+                return;
+        }
+    }
 }
 
 
@@ -68,6 +118,13 @@ public:
     static ConfigVar!(T) lookup(T)(string name,
         /* invariant */ T defaultValue, string description,
         bool dynamic = true, bool automatic = false)
+    in
+    {
+        foreach(c; name) {
+            assert((c >= 'a' && c <= 'z') || c == '.');
+        }
+    }
+    body
     {
         synchronized (Config.classinfo) {
             assert ((name in _vars) is null);
@@ -75,6 +132,23 @@ public:
                 defaultValue, description, dynamic, automatic);
             _vars[name] = var;
             return var;
+        }
+    }
+    
+    static void loadFromEnvironment()
+    {
+        synchronized(Config.classinfo) {
+            foreach(key, val; Environment.get) {
+                toLower(key);
+                replace(key, '_', '.');
+                ConfigVarBase* var = key in _vars;
+                if (var !is null) {
+                    try {
+                        var.fromString(val);
+                    } catch (ConversionException)
+                    {}
+                }
+            }
         }
     }
 
