33b4c452d36d109d3228c1a3907ce3ca1672c8f8
Cody Cutrer <cody@mozy.com>
2009-02-20 19:22:26 -0700

    Set up logging in cat example.

--- mordor/common/examples/cat.d
+++ mordor/common/examples/cat.d
@@ -1,13 +1,19 @@
 module mordor.common.examples.cat;
 
-import tango.io.Stdout;
+import tango.util.log.AppendConsole;
 
+import mordor.common.config;
+import mordor.common.log;
 import mordor.common.scheduler;
 import mordor.common.streams.std;
 import mordor.common.streams.streamtostream;
 
 void main()
 {
+    Config.loadFromEnvironment();
+    Log.root.add(new AppendConsole());
+    enableLoggers();
+
     Stream stdin = new StdinStream;
     Stream stdout = new StdoutStream;
     
