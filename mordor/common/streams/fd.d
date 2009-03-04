12230a04755e5d0b5ef9a25f095bba6838593c76
Cody Cutrer <cody@mozy.com>
2009-03-03 20:47:08 -0700

    Flesh out FDStream.

--- mordor/common/streams/fd.d
+++ mordor/common/streams/fd.d
@@ -1,6 +1,7 @@
 module mordor.common.streams.fd;
 
 import tango.stdc.posix.unistd;
+import tango.stdc.posix.sys.stat;
 import tango.stdc.posix.sys.uio;
 
 public import mordor.common.streams.stream;
@@ -13,8 +14,9 @@ public:
         _fd = fd;
         _own = ownFd;
     }
+    ~this() { close(); } 
     
-    result_t close(CloseType type)
+    result_t close(CloseType type = CloseType.BOTH)
     in
     {
         assert(type == CloseType.BOTH);
@@ -30,6 +32,9 @@ public:
     
     bool supportsRead() { return true; }
     bool supportsWrite() { return true; }
+    bool supportsSeek() { return true; }
+    bool supportsSize() { return true; }
+    bool supportsTruncate() { return true; }
     
     result_t read(Buffer b, size_t len)
     {
@@ -45,6 +50,22 @@ public:
     {
         iovec[] iov = makeIovec(b.readBufs(len));
         return RESULT_FROM_LASTERROR(writev(_fd, iov.ptr, iov.length));
+    }
+    
+    result_t seek(long offset, Anchor anchor, out long pos)
+    {
+        pos = lseek(_fd, offset, cast(int)anchor);
+        if (pos == -1)
+            return RESULT_FROM_LASTERROR();
+        return S_OK;
+    }
+    
+    result_t size(out long size)
+    {
+        stat_t statbuf;
+        int rc = fstat(_fd, &statbuf);
+        size = statbuf.st_size;
+        return RESULT_FROM_LASTERROR(rc);
     }
     
     protected static iovec[] makeIovec(void[][] bufs)
