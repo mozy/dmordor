fb6a4df402c226b74a42db51628cfa15ba301ff0
Cody Cutrer <cody@mozy.com>
2009-03-18 15:13:45 -0600

    Do counts in the scan, instead of outputting detailed file information.

--- mordor/kalypso/examples/scan.d
+++ mordor/kalypso/examples/scan.d
@@ -15,9 +15,10 @@ void main()
     Config.loadFromEnvironment();
     Log.root.add(new AppendConsole());
     enableLoggers();
+    long files, dirs;
     
     void recurse(IObject object, int level) {
-        for(int i = 0; i < level * 4; ++i)
+        /*for(int i = 0; i < level * 4; ++i)
             Stdout.format(" ");
         Stdout.formatln("{}", object["name"].get!(tstring));
         foreach(p; &object.properties) {
@@ -35,9 +36,19 @@ void main()
             for(int i = 0; i < (level + 1) * 4; ++i)
                 Stdout.format(" ");
             Stdout.formatln("#{}", r["name"].get!(tstring));
+        }*/
+        Variant directory = object["directory"];
+        if (directory.isA!(bool) && directory.get!(bool)) {
+            if (++dirs % 1000 == 0) {
+                Stdout.formatln("{} dirs", dirs);
+            }
+        } else {
+            if (++files % 1000 == 0) {
+                Stdout.formatln("{} files", files);
+            }
         }
-        if (level >= 2)
-            return;
+        //if (level >= 2)
+        //    return;
         try {
             foreach (child; &object.children) {
                 recurse(child, level + 1);
@@ -49,5 +60,6 @@ void main()
     
     foreach(vfs; VFSManager.get) {
         recurse(vfs, 0);        
-    }    
+    }
+    Stdout.formatln("{} dirs {} files", dirs, files);
 }
