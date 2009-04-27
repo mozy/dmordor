37ef617dfe7699cc6341896f188a1245eee0901d
Cody Cutrer <cody@mozy.com>
2009-04-27 13:58:15 -0600

    Parse the results of the list command, and callback for each file, instead of returning a stream.

--- mordor/triton/client/list.d
+++ mordor/triton/client/list.d
@@ -6,10 +6,49 @@ import mordor.common.http.client;
 import mordor.common.http.parser;
 import mordor.common.streams.stream;
 import mordor.common.stringutils;
+import mordor.common.xml.parser;
 
-Stream list(ClientConnection conn, string principal, long container,
+void list(Stream xml, void delegate(string, bool) fileDg)
+{
+    string element;
+    string attrib;
+    
+    string path;
+    bool deleted;
+    bool isdir;
+    scope parser = new XmlParser(
+        delegate void(string startTag) {
+            element = startTag.dup;            
+        },
+        delegate void(string endTag) {
+            if (endTag == "NamedObject") {
+                if (path.length == 0)
+                    throw new Exception("No fullpath!");
+                fileDg(path, isdir);
+                path.length = 0;
+                isdir = false;
+            }            
+        },
+        delegate void(string attribName) {
+            attrib = attribName.dup;
+        },
+        delegate void(string attribValue) {
+            if (element == "NamedObject" && attrib == "directory")
+                isdir = to!(bool)(attribValue);
+        },
+        delegate void(string innerText) {
+            if (element == "fullpath")
+                path = innerText.dup;
+        });
+    parser.run(xml);
+    if (parser.error)
+        throw new Exception("Invalid XML");
+}
+
+void list(ClientConnection conn, string principal, long container,
             string prefix, bool recurse, string beginName, long limit,
-            bool includeVersions, bool includeDirectories)
+            bool includeVersions, bool includeDirectories,
+            void delegate(string, bool) fileDg)
 {
     Request requestHeaders;
     requestHeaders.requestLine.uri = "/rest/namedObjects?Prefix=" ~ prefix ~
@@ -22,18 +61,21 @@ Stream list(ClientConnection conn, string principal, long container,
     requestHeaders.entity.extension["X-Emc-Containerid"] = to!(string)(container);
     auto request = conn.request(requestHeaders);
     scope (failure) request.abort();
-    return request.responseStream;
+
+    list(request.responseStream, fileDg);
 }
 
 debug (list)
 {
+    import tango.io.Stdout;
     import tango.net.InternetAddress;
     import tango.util.log.AppendConsole;
-    
+
     import mordor.common.asyncsocket;
     import mordor.common.config;
     import mordor.common.iomanager;
     import mordor.common.log;
+    import mordor.common.streams.file;
     import mordor.common.streams.socket;
     import mordor.common.streams.std;
     import mordor.common.streams.transfer;
@@ -43,6 +85,14 @@ debug (list)
         Config.loadFromEnvironment();
         Log.root.add(new AppendConsole());
         enableLoggers();
+        
+        if (args.length == 3 && args[1] == "-f") {
+            list(new FileStream(args[2], FileStream.Flags.READ),
+                delegate void(string path, bool isdir) {
+                    Stdout.formatln("File '{}' is dir {}", path, isdir);
+                });
+            return;
+        }
     
         IOManager ioManager = new IOManager();
     
@@ -71,10 +121,13 @@ debug (list)
                 includeVersions = to!(bool)(args[9]);
             if (args.length > 10)
                 includeDirectories = to!(bool)(args[10]);
-            transferStream(list(conn, args[3], to!(long)(args[4]), prefix, recurse, beginName, limit, includeVersions, includeDirectories), new StdoutStream());
+            list(conn, args[3], to!(long)(args[4]), prefix, recurse, beginName, limit, includeVersions, includeDirectories,
+                delegate void(string path, bool isdir) {
+                    Stdout.formatln("File '{}' is dir {}", path, isdir);
+                });
     
             ioManager.stop();
-        }, 128 * 1024));
+        }, 64 * 1024));
     
         ioManager.start(true);
     }
