0db15bb48efd2d0e655119b341b04a16cc901084
Cody Cutrer <cody@mozy.com>
2009-05-03 03:10:05 +0000

    Folder-wise access to SqliteManifest

--- mordor/triton/py/manifest.d
+++ mordor/triton/py/manifest.d
@@ -55,8 +55,8 @@ struct PatchInfo
                 pi.filesize = to!(long)(fields[3]);
                 pi.flags = to!(int)(fields[4]);
                 hashstring = fields[5];
-                pi.attribs = to!(int)(fields[6]);
-                pi.patchsize = to!(long)(fields[7]);
+                pi.patchsize = to!(long)(fields[6]);
+                pi.attribs = to!(int)(fields[7]);
                 break;
             default:
                 throw new ConversionException("Invalid patchline " ~ patchline);                
@@ -183,12 +183,50 @@ void parseManifest(Stream stream,
         onStoredFile(file, patches);
     }
 }
-
+import tango.io.Stdout;
 class SqliteManifest
 {
-    this(string file)
+    this(string file, char slash = '/')
     {
+        _slash = slash;
         _db = new Database(file);
+        _db.createFunction("HasAdditionalSlash", 2,
+            delegate void (Result result, Value[] args)
+            in
+            {
+                assert(args.length == 2);
+            }
+            body
+            {
+                string str = args[1].get!(string)();
+                if (str.length == 0) {
+                    result = 0;
+                    return;
+                } else {
+                    size_t ret = locate(str, _slash, args[0].get!(int)());
+                    if (ret == str.length) {
+                        result = 0;
+                        return;
+                    } else {
+                        result = ret;
+                        return;
+                    }
+                }
+            });
+        _db.createFunction("Child", 2,
+            delegate void (Result result, Value[] args)
+            in
+            {
+                assert(args.length == 2);
+            }
+            body
+            {
+                string str = args[0].get!(string)();
+                size_t len = args[1].get!(size_t)();
+                size_t slash = locate(str, _slash, len);
+                Stdout.formatln("str '{}' length {} slash {} str.length {}", str, len, slash, str.length);
+                result = str[len..slash];
+            });
     }
     
     void parse(Stream stream)
@@ -226,22 +264,124 @@ class SqliteManifest
                 insertUser.executeUpdate();
             });
     }
+import tango.io.Stdout;
+    PatchInfo[] find(string file)
+    {
+        if (_findFile is null) {
+            _findFile = _db.prepare("SELECT btime, mtime, ctime, filesize, patchsize, flags, hash, attribs "
+                "FROM patches INNER JOIN files ON files.id=file_id WHERE path=? ORDER BY btime");
+        }
+        synchronized (_findFile) {
+            Stdout.formatln("prepared");
+            _findFile[1] = file;
+            PatchInfo[] result;
+            auto results = _findFile.execute();
+            Stdout.formatln("executed");
+            while (results.next()) {
+                Stdout.formatln("got patch");
+                result ~= PatchInfo(
+                    results.opIndex!(int)(0),
+                    results.opIndex!(int)(1),
+                    results.opIndex!(int)(2),
+                    results.opIndex!(long)(3),
+                    results.opIndex!(long)(4),
+                    results.opIndex!(int)(5),
+                    cast(ubyte[])results.opIndex!(void[])(6),
+                    results.opIndex!(int)(7)
+                    );
+            }
+            Stdout.formatln("complete");
+            return result;
+        }
+    }
+
+    bool dirExists(string path)
+    {
+        if (_dirExists is null) {
+            _dirExists = _db.prepare("SELECT NULL FROM files WHERE path>=? AND path<? LIMIT 1");
+        }
+        string path2;
+        if (path.length == 0)
+            return true;
+        if (path[$-1] != _slash)
+            path ~= _slash;
+        path2 = path.dup;
+        ++path2[$-1];
+        synchronized (_dirExists) {
+            Stdout.formatln("'{}', '{}'", path, path2);
+            _dirExists[1] = path;
+            _dirExists[2] = path2;
+            auto results = _dirExists.execute();
+            scope (exit) delete results;
+            if (results.next()) {
+                Stdout.formatln("exists");
+                return true;
+            } else {
+                Stdout.formatln("doesn't exist");
+                return false;
+            }
+        }
+    }
+    
+    private class ChildrenClosure
+    {
+        string path;
+        int run(int delegate(ref string) dg)
+        {
+            int ret;
+            auto stmt = _db.prepare("SELECT * FROM files WHERE path >= ?1 AND path < ?2");
+            scope (exit) delete stmt;
+            string path2;
+            if (path.length == 0 && _slash == '\\') {
+                path2 ~= 'Z' + 1;
+            } else {
+                if (path.length == 0)
+                    path ~= _slash;
+                else if (path[$-1] != _slash)
+                    path ~= _slash;
+                path2 = path.dup;
+                ++path2[$-1];
+            }
+            stmt[1] = path;
+            stmt[2] = path2;
+            //stmt[3] = path.length;
+            Stdout.formatln("'{}', '{}', {}", path, path2, path.length);
+            auto results = stmt.execute();
+            scope (exit) delete results;
+            while (results.next()) {
+                string p = results.opIndex!(string)(0);
+                if ((ret = dg(p)) != 0) return ret;
+            }
+            return 0;
+        }
+    }
+    
+    int delegate(int delegate(ref string)) children(string path)
+    {
+        auto closure = new ChildrenClosure();
+        closure.path = path.dup;
+        return &closure.run;        
+    }
 
 private:
     Database _db;
+    PreparedStatement _findFile, _dirExists;
+    char _slash;
 }
 
-import mordor.common.streams.buffered;
-import mordor.common.streams.file;
-import mordor.common.streams.std;
-
-void main(string[] args)
-{
-    if (args.length == 2) {
-        scope manifest = new SqliteManifest(args[1]);
-        manifest.parse(new BufferedStream(new StdinStream()));
-    } else if (args.length == 3) {
-        scope manifest = new SqliteManifest(args[2]);
-        manifest.parse(new BufferedStream(new FileStream(args[1], FileStream.Flags.READ)));
+debug(parsemanifest) {
+    import mordor.common.streams.buffered;
+    import mordor.common.streams.file;
+    import mordor.common.streams.std;
+    
+    void main(string[] args)
+    {
+        if (args.length == 2) {
+            scope manifest = new SqliteManifest(args[1]);
+            manifest.parse(new BufferedStream(new StdinStream()));
+        } else if (args.length == 3) {
+            scope manifest = new SqliteManifest(args[2]);
+            manifest.parse(new BufferedStream(new FileStream(args[1], FileStream.Flags.READ)));
+        }
     }
 }
